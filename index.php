b3aa17d8addb04da3a26ce35352f5467aabdbac5
imrodmartin <rod@imrodmartin.com>
2018-11-29 10:37:36 -0500

    Revert "one more comment added"
    This reverts commit e662be604d7fda0119dd7bb87e6086f6874d9526.

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 
 // main page for calculating how many days you've been alive
 // created by Rod Martin - @imrodmartin
-// testing git too... :)
 
-        date_default_timezone_set('America/Detroit');
+        date_default_timezone_set('America/Detroit'); 
 
 if(isset($_POST['submit'])  > 0)
 {
@@ -17,17 +16,17 @@ if(isset($_POST['submit'])  > 0)
   $two = 2000 - $ndays;
   $five = 5000 - $ndays;
   $ten = 10000 - $ndays;
-  $nineteen = 19000 - $ndays;
-  $twenty = 20000 - $ndays;
-  $thirty = 30000 - $ndays;
+  $nineteen = 19000 - $ndays; 
+  $twenty = 20000 - $ndays; 
+  $thirty = 30000 - $ndays; 
 
-  $tw = number_format(2000/365.25, 0, '.','');
-  $fi =number_format(5000/365.25, 0, '.','');
+  $tw = number_format(2000/365.25, 0, '.',''); 
+  $fi =number_format(5000/365.25, 0, '.',''); 
   $te=number_format(10000/365.25, 0, '.','');
-  $ni = number_format(19000/365.25, 0, '.','');
-  $twe=number_format(20000/365.25, 0, '.','');
+  $ni = number_format(19000/365.25, 0, '.',''); 
+  $twe=number_format(20000/365.25, 0, '.',''); 
   $t3=number_format(30000/365.25, 0, '.','');
-
+  
   $ndays = number_format($ndays,0,'.',',');
 
  $contentdiv = '<div style="margin: 0px auto; width: 100%;"><div style="font-size: 30px;">You have been alive for ' .$ndays .' days.</div>' .'<br /><br />'
@@ -60,28 +59,28 @@ if(isset($_POST['submit'])  > 0)
 	<?php echo $contentdiv; ?>
 	<img id="top" src="top.png" style="width: 100%" alt="">
 	<div id="form_container" style="width:100%;">
-
+	
 		<h1><a>My Days</a></h1>
 		<form id="form_551998" class="appnitro"  method="post" action="<?=$_SERVER['PHP_SELF']?>">
 					<div class="form_description">
 			<h2>My Days</h2>
 			<p>Enter your birthday and find out how many days you've lived!</p>
-		</div>
+		</div>						
 			<ul >
-
+	
 			<li>
 <?php
 echo date_picker("bday")?>
 
-			</li>
+			</li>			
 
 
 					<li class="buttons">
-
+			  			    
 				<input id="submit" class="button_text" type="submit" name="submit" value="Submit" />
 		</li>
 			</ul>
-		</form>
+		</form>	
 	</div>
 	</body>
 </html>
@@ -90,7 +89,7 @@ echo date_picker("bday")?>
 		function date_picker($name, $startyear=NULL, $endyear=NULL)
 {
     if($startyear==NULL) $startyear = date("Y");
-    if($endyear==NULL) $endyear=date("Y")-100;
+    if($endyear==NULL) $endyear=date("Y")-100; 
 
     $months=array('','January','February','March','April','May',
     'June','July','August', 'September','October','November','December');
@@ -103,7 +102,7 @@ echo date_picker("bday")?>
        $html.="<option value='$i'>$months[$i]</option>";
     }
     $html.="</select> ";
-
+   
     // Day dropdown
     $html.="<select name=\"".$name."day\">";
     for($i=1;$i<=31;$i++)
@@ -116,7 +115,7 @@ echo date_picker("bday")?>
     $html.="<select name=\"".$name."year\">";
 
     for($i=$startyear;$i>=$endyear;$i--)
-    {
+    {      
       $html.="<option value='$i'>$i</option>";
     }
     $html.="</select> ";
